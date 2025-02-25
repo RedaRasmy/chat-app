dd7717867095b233e5dfddbdb44d85dc58897b1b
RedaRasmy <red7mer@gmail.com>
2025-02-25 18:11:54 +0000

    clean up

--- env.ts
+++ env.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-    NEXTAUTH_SECRET: z.string().min(1),
-    MONGODB_URI: z.string().min(1),
     ADMIN_EMAIL: z.string().min(1),
 });
 
--- src/app/components/SideBar/Header/UserLabel.tsx
+++ src/app/components/SideBar/Header/UserLabel.tsx
@@ -1,24 +1,17 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
-import { getServerSession } from "next-auth";
-import React from "react";
 
 export default async function UserLabel() {
 
-    const session = await getServerSession()
     
-    const user = session?.user
-    
-    console.log(user)
-
-    if (user) return (
+    return (
         <div className="flex items-center gap-2 p-2 mb-2 cursor-pointer">
             <Avatar >
-                <AvatarImage src={user.image ?? ''}/>
-                <AvatarFallback>{user.name?.charAt(0)}</AvatarFallback>
+                <AvatarImage src={''}/>
+                <AvatarFallback>{}</AvatarFallback>
             </Avatar>
-            <h1 className="font-semibold tracking-wider text-lg">{user.name} Reda</h1>
-            {user.role === 'admin' && <p className="text-yellow-600">(admin)</p>}
+            <h1 className="font-semibold tracking-wider text-lg">{} Reda</h1>
+            {true && <p className="text-yellow-600">(admin)</p>}
         </div>
     )
 }
