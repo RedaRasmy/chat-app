cd2a11e7273041e6da923a97ca345b39418fbef6
RedaRasmy <red7mer@gmail.com>
2025-03-10 19:29:55 +0000

    init cypress

--- src/zustand/chatsStore.ts
+++ src/zustand/chatsStore.ts
@@ -2,6 +2,7 @@ import { addChat, addMessage } from "@/actions";
 import { FullChat, SChat, SMessage } from "@/db/types";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
+import {persist} from 'zustand/middleware'
 
 type ChatsState = {
     chats: FullChat[] | undefined;
@@ -25,55 +26,60 @@ type ChatsActions = {
 };
 
 export const useChatsStore = create<ChatsState & ChatsActions>()(
-    immer((set) => ({
-        chats: undefined,
-        isLoading: false,
-        setChats: (chats) =>
-            set({
-                chats,
-            }),
-        addNewChat: async (participant2) => {
-            set({ isLoading: true });
-            try {
-                const newChat = await addChat(participant2);
-                set({ isLoading: false });
-                if (newChat) {
-                    set((state) => ({
-                        chats: state.chats ? [...state.chats, newChat] : [newChat],
-                    }));
-                    return newChat
-                }
-            } catch (error) {
-                console.error("Failed to add new chat : ", error);
-            } finally {
-                set({ isLoading: false });
-            }
-        },
-        addNewMessage: async ({chatId, userId, content}) => {
-            try {
+    persist(
+        immer((set) => ({
+            chats: undefined,
+            isLoading: false,
+            setChats: (chats) =>
+                set({  
+                    chats,
+                }),
+            addNewChat: async (participant2) => {
                 set({ isLoading: true });
-                const newMessage = await addMessage({userId, chatId, content});
-                set({ isLoading: false });
-                set(state => {
-                    const chat = state.chats?.find(chat=>chat.id===chatId)
+                try {
+                    const newChat = await addChat(participant2);
+                    set({ isLoading: false });
+                    if (newChat) {
+                        set((state) => ({
+                            chats: state.chats ? [...state.chats, newChat] : [newChat],
+                        }));
+                        return newChat
+                    }
+                } catch (error) {
+                    console.error("Failed to add new chat : ", error);
+                } finally {
+                    set({ isLoading: false });
+                }
+            },
+            addNewMessage: async ({chatId, userId, content}) => {
+                try {
+                    set({ isLoading: true });
+                    const newMessage = await addMessage({userId, chatId, content});
+                    set({ isLoading: false });
+                    set(state => {
+                        const chat = state.chats?.find(chat=>chat.id===chatId)
+                        if (chat) {
+                            chat.messages.push(newMessage)
+                        }
+                    })
+                    return newMessage
+                } catch (error) {
+                    console.error("Failed to add new Message : ", error);
+                } finally {
+                    set({ isLoading: false });
+                }
+            },
+            addReceivedMessage : ({message,chatId}) => {
+                set((state)=>{
+                    const chat = state.chats?.find(chat=>chat.id === chatId)
                     if (chat) {
-                        chat.messages.push(newMessage)
+                        chat.messages.push(message)
                     }
                 })
-                return newMessage
-            } catch (error) {
-                console.error("Failed to add new Message : ", error);
-            } finally {
-                set({ isLoading: false });
             }
-        },
-        addReceivedMessage : ({message,chatId}) => {
-            set((state)=>{
-                const chat = state.chats?.find(chat=>chat.id === chatId)
-                if (chat) {
-                    chat.messages.push(message)
-                }
-            })
+        })),
+        {
+            name: 'chats'
         }
-    }))
+    )
 );
