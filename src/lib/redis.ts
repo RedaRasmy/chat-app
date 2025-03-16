690984f7f81016236ddded1e87b0c11ef1651399
RedaRasmy <red7mer@gmail.com>
2025-03-16 23:09:23 +0000

    clean cypress

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,11 +1,11 @@
-import {Redis} from 'ioredis'
+// import {Redis} from 'ioredis'
 
-const getRedisUrl = () => {
-    if (process.env.REDIS_URL) {
-        return process.env.REDIS_URL
-    }
-    throw new Error('REDIS_URL is not defined')
-}
+// const getRedisUrl = () => {
+//     if (process.env.REDIS_URL) {
+//         return process.env.REDIS_URL
+//     }
+//     throw new Error('REDIS_URL is not defined')
+// }
 
-// redis token ??
-export const redis = new Redis(getRedisUrl())
+// // redis token ??
+// export const redis = new Redis(getRedisUrl())
