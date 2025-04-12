67ef88fd19bc75ee6c06434bf2b88263d870e84f
RedaRasmy <red7mer@gmail.com>
2025-04-12 20:32:21 +0100

    update server.js for deployment

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import next from "next"
 import { Server } from "socket.io"
 
 const dev = process.env.NODE_ENV !== "production"
-const hostname = "localhost"
-const port = 3000
+// const hostname = "localhost"
+const port = process.env.PORT || 3000
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
+const app = next({ dev, port })
 const handler = app.getRequestHandler()
 
 app.prepare().then(() => {
@@ -45,13 +45,8 @@ app.prepare().then(() => {
             process.exit(1)
         })
         .listen(port, () => {
-            console.log(`> Ready on http://${hostname}:${port}`)
+            console.log(`lisen on port : ${port}`)
         })
 })
 
 
-// export type WSResponse = {
-//     status: "ok" | "not ok";
-// };
-
-// export type WSCallback = (response: WSResponse) => void;
