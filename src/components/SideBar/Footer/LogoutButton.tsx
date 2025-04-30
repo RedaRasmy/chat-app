3697c01b9d33c5c8c0fc8db7299ea5c3bca2116a
RedaRasmy <red7mer@gmail.com>
2025-04-30 12:20:52 +0100

    clear localstorage on logout

--- src/components/SideBar/Footer/LogoutButton.tsx
+++ src/components/SideBar/Footer/LogoutButton.tsx
@@ -1,14 +1,20 @@
-
 import { logout } from "@/app/server-actions/auth-actions"
 import { Button } from "@/components/ui/button"
 import { LogOut } from "lucide-react"
 
 export default function LogoutButton() {
     return (
-        <Button 
-            onClick={async ()=>{await logout()}}
-            className="flex items-center cursor-pointer justify-center gap-2 m-2 w-fit" variant={'ghost'} >
-            <LogOut/>
+        <Button
+            onClick={async () => {
+                await logout()
+                localStorage.removeItem("chats")
+                localStorage.removeItem("messages")
+                localStorage.removeItem("currentChatId")
+            }}
+            className="flex items-center cursor-pointer justify-center gap-2 m-2 w-fit"
+            variant={"ghost"}
+        >
+            <LogOut />
             Logout
         </Button>
     )
