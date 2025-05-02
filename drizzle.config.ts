93cb8effbcf7e47846dc3be2b69d447e2b6fe7d8
RedaRasmy <red7mer@gmail.com>
2025-05-02 22:57:25 +0100

    little change

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,7 @@ import {config} from 'dotenv'
 config({ path: ".env.local" });
 
 import { defineConfig } from "drizzle-kit";
+import { env } from './env';
 
 
 export default defineConfig({
@@ -9,6 +10,6 @@ export default defineConfig({
     out: "./src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: env.DATABASE_URL,
     },
 });
